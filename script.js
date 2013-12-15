e6c0380c4edb08278e4ea5363d82b4fc44cb8874
WooTay <taylorwoods13@me.com>
2013-12-15 16:30:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -49,11 +49,57 @@ $(document).ready(function(){
 							
 							console.log(nutritionalData);
 
-							/* 							
-							if((nutritionalData.calories > 300) && (nutritionalData.calories < 600)) {
-								//do ridiculous stuff
-							}
-							*/
+							 /*if(nutritionalData.calories === 0) {
+								$(nutritionalData.name).replaceWith("Testing1");
+								
+								} else if (nutritionalData.calories > 15) && (nutritionalData.calories < 300) {
+            					  			$(nutritionalData.name).replaceWith("Testing2");
+            					  
+            							} else if (nutritionalData.calories > 310) && (nutritionalData.calories < 500) {
+            					  			$(nutritionalData.name).replaceWith("Testing3");
+            					  
+            							} else if (nutritionalData.calories > 510) && (nutritionalData.calories < 1090) {
+            					  			$(nutritionalData.name).replaceWith("Testing4");*/
+            					  
+            					  
+							 /*if(nutritionalData.protein === 0) {
+								$(nutritionalData.name).replaceWith("Testing5");
+								
+								} else if (nutritionalData.protein > 1) && (nutritionalData.calories < 15) {
+            					  			$(nutritionalData.name).replaceWith("Testing5");
+            					  
+            							} else if (nutritionalData.protein > 16) && (nutritionalData.calories < 30) {
+            					  			$(nutritionalData.name).replaceWith("Testing7");
+            					  
+            							} else if (nutritionalData.protein > 31) && (nutritionalData.calories < 48) {
+            					  			$(nutritionalData.name).replaceWith("Testing8");*/
+            					  
+            					  
+            						/*if(nutritionalData.totalFat === 0) {
+								$(nutritionalData.name).replaceWith("Testing9");
+								
+								} else if (nutritionalData.totalFat > 0.5) && (nutritionalData.calories < 15) {
+            								$(nutritionalData.name).replaceWith("Testing10");
+            					  
+            							} else if (nutritionalData.totalFat > 14) && (nutritionalData.calories < 31) {
+            					  			$(nutritionalData.name).replaceWith("Testing11");
+            					  
+            							} else if (nutritionalData.totalFat > 32) && (nutritionalData.calories < 56) {
+            					  			$(nutritionalData.name).replaceWith("Testing12");*/
+								
+								
+							/*if(nutritionalData.sodium === 0) {
+								$(nutritionalData.name).replaceWith("Testing13");
+								
+								} else if (nutritionalData.sodium > 4) && (nutritionalData.calories < 30) {
+            					  			$(nutritionalData.name).replaceWith("Testing14");
+            					  
+            							} else if (nutritionalData.sodium > 31) && (nutritionalData.calories < 60) {
+            					  			$(nutritionalData.name).replaceWith("Testing15");
+            					  
+            							} else if (nutritionalData.sodium > 61) && (nutritionalData.calories < 111) {
+            					  			$(nutritionalData.name).replaceWith("Testing16");*/
+								 
 						}
 					
 					});
