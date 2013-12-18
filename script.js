f2f0baf74d0a2347e4f94c433976ce91508dd08c
WooTay <taylorwoods@Taylors-MacBook-Pro.local>
2013-12-17 22:46:19 -0500

    Working through the hard stuff

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+
 	var nutritionType = {
 	
 		"1":"calories",
@@ -9,6 +10,7 @@ $(document).ready(function(){
 		"5":"sodium"
 	
 	};
+
 
 	var dataUrl = "http://www.mcdonalds.com/getnutrition/US_Menu.xml";
 	$.get(dataUrl,function (data, status) {
@@ -25,12 +27,13 @@ $(document).ready(function(){
 			$(".product_item").each(function(index, productItem){
 		
 
-			
-				$(productItem).hover(function(){
+				var $productItem = $(productItem);
+				$productItem.hover(function(){
 
-					var productName = $(productItem).find("center").text();
+					var productName = $productItem.find("center").text();
 					
 					$(menuJson.products.product).each(function(index, menuItem){
+					
 					
 						if(menuItem.name === productName) {
 							console.log(index);
@@ -42,21 +45,98 @@ $(document).ready(function(){
 							var nutritionalData = {};
 							
 							$(rawNutritionData).each(function(index, currentNutritionalData){
-								if(currentNutritionalData.type == "1") {
+								if(currentNutritionalData.type === "1") {
 									nutritionalData.calories = currentNutritionalData.value;
 								}
 							});
 							
-							console.log(nutritionalData);
+							 console.log(nutritionalData);
+							
+							
+							
+								//calories value
+								//$(productItem).find("tr:nth-child(2) td:nth-child(2)").html("Low Self-Esteem"); 
+								
+							//$(productItem).find("td.notes").html("coool");
 
-							/* 							
-							if((nutritionalData.calories > 300) && (nutritionalData.calories < 600)) {
-								//do ridiculous stuff
-							}
-							*/
+							//data cleanup
+							nutritionalData.calories = parseInt(nutritionalData.calories);
+							
+							if((nutritionalData.calories == 0)) {
+								$productItem.find("tr:nth-child(2) td.red_bold").html("Low Self-Esteem"); 
+								
+							} else if ((nutritionalData.calories > 15) && (nutritionalData.calories < 300)) {
+							  $productItem.find("tr:nth-child(2) td.red_bold").html("Muscle Cramps");
+							  
+							} else if ((nutritionalData.calories > 310) && (nutritionalData.calories < 500)) {
+							  $productItem.find("tr:nth-child(2) td.red_bold").html("Nausea or Vomiting");
+							  
+							} else if ((nutritionalData.calories > 510) && (nutritionalData.calories < 1090)) {
+							  $productItem.find("tr:nth-child(2) td.red_bold").html("Heart Attack");
+							
+							}  
+            					  
+            				/*if((nutritionalData.totalFat == 0)) {
+								$(productItem).find("tr:nth-child(3) td.red_bold").html("Low Self-Esteem");
+								
+								} else if ((nutritionalData.totalFat > 0.5) && (nutritionalData.calories < 15)) {
+            					  $(productItem).find("tr:nth-child(3) td.red_bold").html("Muscle Cramps");
+            					  
+            					} else if ((nutritionalData.totalFat > 14) && (nutritionalData.calories < 31)) {
+            					  $(productItem).find("tr:nth-child(3) td.red_bold").html("Nausea or Vomiting");
+            					  
+            					} else if ((nutritionalData.totalFat > 32) && (nutritionalData.calories < 56)) {
+            					  $(productItem).find("tr:nth-child(3) td.red_bold").html("Heart Attack");
+            					
+            					}  
+            					  
+            				if((nutritionalData.carbohydrates == 0)) {
+								$(productItem).find("tr:nth-child(4) td.red_bold").html("Low Self-Esteem");
+								
+								} else if ((nutritionalData.carbohydrates > 4) && (nutritionalData.calories < 30)) {
+            					  $(productItem).find("tr:nth-child(4) td.red_bold").html("Muscle Cramps");
+            					  
+            					} else if ((nutritionalData.carbohydrates > 31) && (nutritionalData.calories < 60)) {
+            					  $(productItem).find("tr:nth-child(4) td.red_bold").html("Nausea or Vomiting");
+            					  
+            					} else if ((nutritionalData.carbohydrates > 61) && (nutritionalData.calories < 111)) {
+            					  $(productItem).find("tr:nth-child(4) td.red_bold").html("Heart Attack");
+            					
+            					}  
+            					  
+							 if((nutritionalData.protein == 0)) {
+								$(productItem).find("tr:nth-child(5) td.red_bold").html("Low Self-Esteem");
+								
+								} else if ((nutritionalData.protein > 1) && (nutritionalData.calories < 15)) {
+            					  $(productItem).find("tr:nth-child(5) td.red_bold").html("Muscle Cramps");
+            					  
+            					} else if ((nutritionalData.protein > 16) && (nutritionalData.calories < 30)) {
+            					  $(productItem).find("tr:nth-child(5) td.red_bold").html("Nausea or Vomiting");
+            					  
+            					} else if ((nutritionalData.protein > 31) && (nutritionalData.calories < 48)) {
+            					  $(productItem).find("tr:nth-child(5) td.red_bold").html("Heart Attack");  
+            					
+            					} 
+				
+							if((nutritionalData.sodium == 0)) {
+								$(productItem).find("tr:nth-child(6) td.red_bold").html("Low Self-Esteem");
+								
+								} else if ((nutritionalData.sodium > 10) && (nutritionalData.calories < 240)) {
+            					  $(productItem).find("tr:nth-child(6) td.red_bold").html("Muscle Cramps");
+            					  
+            					} else if ((nutritionalData.sodium > 280) && (nutritionalData.calories < 990)) {
+            					  $(productItem).find("tr:nth-child(6) td.red_bold").html("Nausea or Vomiting");
+            					  
+            					} else if ((nutritionalData.sodium > 1030) && (nutritionalData.calories < 2150)) {
+            					  $(productItem).find("tr:nth-child(6) td.red_bold").html("Heart Attack");
+            					            				
+								 
+							}*/
+							
 						}
 					
 					});
+					
 					
 				});
 
@@ -66,54 +146,8 @@ $(document).ready(function(){
 			console.log("timeout fired");
 			
 		},1500);
-	
+
 	});
 	
-// (function( window, undefined ) {
-// 
-// var pluginName = 'defaultPluginName',
-//         defaults = {
-//             propertyName: "value"
-//             
-//             
-//             
-            
-            
-            
-            
-            //attempt to change text in Hover Information
-            //text should be same style,color,(size?) as original (put in css?)
-            //(daily value) should be changed to (symptoms include) 
-            
-            /*Calories: 0 |15-300| 310-500 |510-1090| 
-            if calories are ===0 then " Low Self-Esteem ", 
-            else if calories are b/t 15-300 " Muscle Cramps or Spasms", 
-            else if calories are b/t 310-500 " Nausea or Vomiting",
-            else if calories are b/t 510-1090 " Heart Attack " */
-            
-            /*Protein(g): 0 |1-15| 16-30 |31-48|
-            if Protein is ===0 then " Low Self-Esteem ", 
-            else if Protein is b/t 1-15(g) " Muscle Cramps or Spasms", 
-            else if Protein is b/t 16-30(g) " Nausea or Vomiting",
-            else if Protein is b/t 31-48(g) " Heart Attack " */
-            
-            /*Total Fat(g): 0 |0.5-13| 14-31 |32-56|
-            if Total Fat is ===0 then " Low Self-Esteem ", 
-            else if Total Fat is b/t 0.5-13(g) " Muscle Cramps or Spasms", 
-            else if Total Fat is b/t 14-31(g) " Nausea or Vomiting",
-            else if Total Fat is b/t 32-56(g) " Heart Attack " */
-            
-            /*Total Carb(g): 0 |4-30| 31-60 |61-111|
-            if Total Carb is ===0 then " Low Self-Esteem ", 
-            else if Total Carb is b/t 4-30(g) " Muscle Cramps or Spasms", 
-            else if Total Carb is b/t 31-60(g) " Nausea or Vomiting",
-            else if Total Carb is b/t 61-111(g) " Heart Attack " */
-            
-            /*Sodium(mg): 0 |10-240| 280-990 |1030-2150| 
-            if Sodium is ===0 then " Low Self-Esteem ", 
-            else if Sodium is b/t 10-240(g) " Muscle Cramps or Spasms", 
-            else if Sodium is b/t 280-990(g) " Nausea or Vomiting",
-            else if Sodium is b/t 1030-2150(g) " Heart Attack " */
-            	
 	
 });
